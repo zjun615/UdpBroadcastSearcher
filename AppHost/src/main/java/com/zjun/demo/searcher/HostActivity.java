6e9efed1bf5f1d4bc2bb0fc0cd5308661856d84d
Ralap Zhong <zjundswzu@126.com>
2016-09-23 19:26:11 +0800

    第一次提交

---
+++ AppHost/src/main/java/com/zjun/demo/searcher/HostActivity.java
@@ -0,0 +1,123 @@
+package com.zjun.demo.searcher;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.zjun.searcher.SearcherHost;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 搜索主机 —— 简单demo
+ */
+public class HostActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int MESSAGE_SEARCH_START= 1;
+    private static final int MESSAGE_SEARCH_FINISH = 2;
+
+    private Button btn_search;
+    private TextView tv_device;
+
+    private List<SearcherHost.DeviceBean> mDeviceList = new ArrayList<>();
+
+    private MyHandler mHandler = new MyHandler(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+    }
+
+    private void initData() {
+    }
+
+    private void initView() {
+        btn_search = $(R.id.btn_search);
+        tv_device = $(R.id.tv_device);
+
+        btn_search.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_search:
+                startToSearch();
+        }
+    }
+
+    /**
+     * 开始搜索
+     */
+    private void startToSearch() {
+        new SearcherHost() {
+            @Override
+            public void onSearchStart() {
+                pushMessage(MESSAGE_SEARCH_START);
+            }
+
+            @Override
+            public void onSearchFinish(Set deviceSet) {
+                mDeviceList.clear();
+                mDeviceList.addAll(deviceSet);
+                pushMessage(MESSAGE_SEARCH_FINISH);
+            }
+
+            @Override
+            public void printLog(String log) {
+                Log.i(SearcherHost.class.getSimpleName(), "Searching..." + log);
+            }
+        }.search();
+    }
+
+    private void pushMessage(int what) {
+        mHandler.sendEmptyMessage(what);
+    }
+
+    /**
+     * Handler
+     */
+    private static class MyHandler extends Handler {
+        private WeakReference<HostActivity> ref;
+
+        MyHandler(HostActivity activity) {
+            ref = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            HostActivity activity = ref.get();
+            switch (msg.what) {
+                case MESSAGE_SEARCH_START:
+                    activity.btn_search.setText("正在搜索...");
+                    activity.tv_device.setText("");
+                    break;
+                case MESSAGE_SEARCH_FINISH:
+                    activity.btn_search.setText("搜索");
+                    for (SearcherHost.DeviceBean d : activity.mDeviceList) {
+                        String show = d.getIp() + " : " + d.getPort();
+                        activity.tv_device.append(show + "\n\n");
+                    }
+
+            }
+        }
+    }
+
+    private <V extends View> V $(int id) {
+        return (V) findViewById(id);
+    }
+}
---
+++ se_device/src/main/java/com/zjun/demo/device/DeviceClass.java
@@ -0,0 +1,138 @@
+package com.zjun.demo.device;
+
+import com.zjun.searcher.SearcherConst;
+import com.zjun.searcher.SearcherDevice;
+
+import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 设备 —— 携带用户自定义数据的demo
+ */
+public class DeviceClass {
+    private static final byte DEVICE_TYPE_NAME_21 = 0x21;
+    private static final byte DEVICE_TYPE_ROOM_22 = 0x22;
+
+    public static void main(String[] args) {
+        SearcherDevice searcherDevice = new SearcherDevice(1024) {
+            @Override
+            public void onDeviceSearched(InetSocketAddress inetSocketAddress) {
+                String hostIP = inetSocketAddress.getAddress().getHostAddress();
+                int hostPort = inetSocketAddress.getPort();
+                printf("I am found by host: " + hostIP + "-" + hostPort);
+            }
+
+            @Override
+            public void printLog(String s) {
+                printf("Device is ready..." + s);
+            }
+
+
+            /**
+             * 重写以下2个方法
+             */
+
+            /**
+             * 响应时的打包数据
+             * * dataType(1) + len(4) + data(n)
+             */
+            @Override
+            protected byte[] packUserData() {
+                String name = "LED灯";
+                String room = "客厅";
+                try {
+                    byte[] nameBytes = name.getBytes("UTF-8");
+                    byte[] roomBytes = room.getBytes("UTF-8");
+                    byte[] data = new byte[5 + nameBytes.length + 5 + roomBytes.length];
+                    int offset = 0;
+
+                    data[offset++] = DEVICE_TYPE_NAME_21;
+                    data[offset++] = (byte) nameBytes.length;
+                    data[offset++] = (byte) (nameBytes.length >> 8);
+                    data[offset++] = (byte) (nameBytes.length >> 16);
+                    data[offset++] = (byte) (nameBytes.length >> 24);
+                    System.arraycopy(nameBytes, 0 , data, offset, nameBytes.length);
+                    offset += nameBytes.length;
+
+                    data[offset++] = DEVICE_TYPE_ROOM_22;
+                    data[offset++] = (byte) roomBytes.length;
+                    data[offset++] = (byte) (roomBytes.length >> 8);
+                    data[offset++] = (byte) (roomBytes.length >> 16);
+                    data[offset++] = (byte) (roomBytes.length >> 24);
+                    System.arraycopy(roomBytes, 0 , data, offset, roomBytes.length);
+
+                    return data;
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                return super.packUserData();
+            }
+
+            /**
+             * 解析用户数据
+             * len(4) + data(n)
+             */
+            @Override
+            public boolean parseUserData(byte type, byte[] userData) {
+                if (userData.length < 4) {
+                    return false;
+                }
+                int len = userData[0] & 0xFF;
+                len |= (userData[1] << 8) & 0xFF00;
+                len |= (userData[2] << 16) & 0xFF0000;
+                len |= (userData[3] << 24) & 0xFF000000;
+
+                if (len != userData.length - 4) {
+                    return false;
+                }
+
+                String content = null;
+                try {
+                    content = new String(userData, 4, len, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                if (content == null) {
+                    return false;
+                }
+
+                switch (type) {
+                    case SearcherConst.PACKET_TYPE_FIND_DEVICE_REQ_10:
+                        printf("主机请求数据：" + content);
+                        break;
+                    case SearcherConst.PACKET_TYPE_FIND_DEVICE_CHK_12:
+                        printf("主机核对数据：" + content);
+                        break;
+                }
+                return true;
+            }
+
+            /**
+             * 判断ip是否是本机ip
+             * @param ip 判断的ip地址
+             * @return
+             */
+            @Override
+            public boolean isOwnIp(String ip) {
+                if (ip == null) {
+                    return false;
+                }
+                String ownIp = null;
+                try {
+                    ownIp = InetAddress.getLocalHost().getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                return ip.equals(ownIp);
+            }
+        };
+
+        searcherDevice.open();
+    }
+
+    private static void printf(String format, Object... objs) {
+        System.out.printf(format + "\n", objs);
+    }
+}
